282535d86239fa1d417f03ee12e50c120707a607
Oleg Salesfloor <wildpenguin@gmail.com>
2026-09-21 21:43:53 -0400

    Add project structure

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessageHub.Controllers;
+
+[ApiController]
+[Route("api/events")]
+public class EventsController : ControllerBase
+{
+    public ActionResult<Event> GetEvents()
+    {
+        return new Event{
+            "Prince Eduard School",
+            "123 ColonelBy drive",
+            "info" // notification type
+        };
+    }
+}
